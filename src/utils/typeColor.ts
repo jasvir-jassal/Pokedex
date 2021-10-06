1a2d17b73015360a1b8de77d3ad83689adac4fa6
Jasvir Jassal <jasvir.jassal@outlook.com>
2021-10-06 20:34:30 +0200

    Add typeColor function
    This function is used for display the types colours of each pokémon

---
+++ src/utils/typeColor.ts
@@ -0,0 +1,40 @@
+export const typesColors = (typeColor: string) => {
+    switch (typeColor) {
+      case "grass":
+        return "#9bcc50";
+      case "poison":
+        return "#b97fc9";
+      case "fire":
+        return "#fd7d24";
+      case "water":
+        return "#4592c4";
+      case "bug":
+        return "#729f3f";
+      case "normal":
+        return "#a4acaf";
+      case "flying":
+        return "#3dc7ef";
+      case "electric":
+        return "#eed535";
+      case "ground":
+        return "#ab9842";
+      case "fairy":
+        return "#fdb9e9";
+      case "fighting":
+        return "#d56723";
+      case "psychic":
+        return "#f366b9";
+      case "rock":
+        return "#a38c21";
+      case "steel":
+        return "#9eb7b8";
+      case "ghost":
+        return "#7b62a3";
+      case "ice":
+        return "#51c4e7";
+      case "dragon":
+        return "#f16e57";
+      case "dark":
+        return "#707070";
+    }
+  };
